9efc368c291389134bfc186c5a23deb650b60a4d
Tomas Reyes <treyes4523@gmail.com>
2018-06-20 12:01:49 -0500

    Added IoT testing files

---
+++ IoT-Testing/huzzah/boot.py
@@ -0,0 +1,10 @@
+#import esp
+#esp.osdebug(None)
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+#this huzzah has a boot.py function, so all commands will run on boot
+#runs the network setup
+import network_huzzah
