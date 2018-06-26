d448a367c5f0fd30ed5986548d531ec49f689c5b
Vincent Nguyen <petervo@main-10-230-184-225.tamulink.tamu.edu>
2018-06-26 09:40:34 -0500

    Server and client code for project 1

---
+++ SU18 Project 1/Team Team/client.py
@@ -0,0 +1,53 @@
+#imports
+import sys
+import socket
+import time
+from machine import Pin,Signal
+import machine
+
+
+# setup the ADC on ADC channel 0 (the labeled ADC on board)
+adc = machine.ADC(0)
+
+# Setup client socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = '192.168.4.1'
+port = 324
+server_address = (address, port)
+print("Connecting to",address,"on port",port)
+
+# Connects to server
+sock.connect(server_address)
+
+# Create an array to hold the past four data values read
+avg = 0
+past4 = [0 ,0 ,0 ,0]
+
+for i in range(0,30):
+	sum = 0
+	# Read in value from ADC pin
+	value = adc.read()
+
+	# Add the value read from ADC pin and remove first value from the array
+	past4.append(value)
+	past4.pop(0)
+
+	# Sum up the 4 values in the array and take their average
+	counter = 0
+	for u in range(0,4):
+		if(past4[u]!=0):
+			sum += past4[u]
+			counter+=1
+	avg = sum / counter
+
+	# Send Instantaneous and Average values to server
+	print ('Instant: '+ str(value)+' Avg: '+str(avg))
+	#sock.sendall('Instant: '+ str(value)+' Avg: '+str(avg))
+	sock.sendall(str(value))
+	time.sleep(0.5)
+	sock.sendall(str(avg))
+	time.sleep(0.5)
+time.sleep(0.5)
+sock.sendall('End of Message')
+time.sleep(0.5)
+sock.sendall('End of Message')
---
+++ SU18 Project 1/Team Team/server.py
@@ -0,0 +1,125 @@
+import socket
+import sys
+global led
+import pycom, binascii
+
+# Run network_pycom then run network_huzzah before running server.py and client.py
+
+#turns off the pycom blue heartbeat
+pycom.heartbeat(False)
+
+#sets the rgbled to a purple color
+pycom.rgbled(0xF333FF)
+
+
+def main():
+
+	# Setup port and socket
+	port = 324
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Bind to port
+	server_address = ('', port)
+	try:
+		sock.bind(server_address)
+	except socket.error as msg:
+		print('Bind Failed. Error code ' + str(msg[0]))
+		sys.exit
+	print('Socket bind complete')
+
+	# Start the listening to incoming connections
+	sock.listen(1)
+	print('Socket now listening')
+	accept_connection(sock,port)
+
+def accept_connection(sock, port):
+	# Start the client acceptance system
+	# This function will run on the successful connection of a client
+
+	# conn is a new socket object able to send/receive data on the accept_connection
+	# addr is the address bound to the socket on the other end of the accept_connection
+	conn,addr = sock.accept()
+	print('Connected with ' + addr[0] + ':' + str(addr[1]))
+	print("client connected :: ",addr)
+	receive_data(conn,sock)
+
+
+def receive_data(conn,sock):
+	# This function attempts to receive data from the client
+	try:
+	# While true, loop to receive data
+		while True:
+			# Receive 16 bytes of data
+			inst = conn.recv(16).decode()
+			avg = conn.recv(16).decode()
+
+			if(inst == 'End of Message' or avg =='End of Message'):
+				# sometimes data will be None, so if there is data, we need to process it
+				print("Inst: " + inst + ' avg: ' + avg)
+				break
+			else:
+				# If 'End of message' is received, stop receiving data from client
+				mappedInst = convertData(float(inst))
+				mappedAvg = convertData(float(avg))
+				ledData(mappedInst)
+				print("Inst:", mappedInst, ' avg:' , mappedAvg)
+	finally:
+		# Close the connection b/w socket then close the socket
+		conn.close()
+		sock.close()
+
+def convertData(oldValue):
+	# This function converts the ADC value to cm
+	# Min and Max of Sensor integers
+	oldMin = 70
+	oldMax = 210
+	oldRange = oldMax - oldMin
+
+	# Min and Max of distance (cm)
+	newMax = 76.2
+	newMin = 12.7
+	newRange = newMax - newMin
+	newVal = (((oldValue - oldMin) * newRange)/ oldRange) + newMin
+
+	return newVal
+
+def ledData(mappedVal):
+	if(mappedVal < 12.7):
+		pycom.rgbled(0x000000)
+	if(12.7 < mappedVal < 20):
+		pycom.rgbled(0xFF00FF)
+	if(20 < mappedVal < 30):
+		pycom.rgbled(0x9000FF)
+	if(30 < mappedVal < 40):
+		pycom.rgbled(0x0000FF)
+	if(40 < mappedVal < 50):
+		pycom.rgbled(0x00FFF2)
+	if(50 < mappedVal < 60):
+		pycom.rgbled(0x10FF00)
+	if(60 < mappedVal < 70):
+		pycom.rgbled(0xF2FF00)
+	if(mappedVal > 70):
+		pycom.rgbled(0xFF0000)
+
+'''
+def process_data(data):
+	#process the data, and change LED based on input
+	if(data == b'led green'):
+		pycom.rgbled(0x007f00)
+	if(data == b'led red'):
+		pycom.rgbled(0x7f0000)
+	if(data == b'led blue'):
+		pycom.rgbled(0x0000ff)
+	if("0x" in data):
+		#all the data is sent in binary mode, which we need to decode to get the string version of the data
+		data = data.decode('utf-8')
+		print(data)
+		#gets rid of the 'led 0x', and typecasts it in base 16 (aka hex)
+		color = int(data[data.index("x")+1:],16)
+		#sets the color
+		pycom.rgbled(color)
+	return False
+'''
+
+#runs the main function on an import
+main()
